0a76eb8e67a52237e51aceeef742bd2bf8ecf1dd
rafaelpenablesa <89039366+rafaelpenablesa@users.noreply.github.com>
2024-07-28 18:10:21 -0600

    agregue Admin credentials

--- Tercera_preentrega_penaRafael/biblioteca/admin.py
+++ Tercera_preentrega_penaRafael/biblioteca/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Autor, Libro, Editorial
 
-# Register your models here.
+admin.site.register(Autor)
+admin.site.register(Libro)
+admin.site.register(Editorial)
