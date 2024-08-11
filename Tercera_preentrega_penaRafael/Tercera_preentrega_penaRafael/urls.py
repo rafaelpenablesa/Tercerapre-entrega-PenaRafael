c681a037acaca75589eda68eb36078574bfa9845
rafaelpenablesa <89039366+rafaelpenablesa@users.noreply.github.com>
2024-08-11 07:48:27 -0600

    detalles log in

--- Tercera_preentrega_penaRafael/Tercera_preentrega_penaRafael/urls.py
+++ Tercera_preentrega_penaRafael/Tercera_preentrega_penaRafael/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('biblioteca/', include('biblioteca.urls')),
     path('blogs/', include('blogs.urls')),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
