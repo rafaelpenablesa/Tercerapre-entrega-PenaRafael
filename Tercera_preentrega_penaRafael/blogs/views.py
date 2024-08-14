ef96821db3b7e210928e9a17d78ad05207e94df4
rafaelpenablesa <89039366+rafaelpenablesa@users.noreply.github.com>
2024-08-13 22:37:29 -0600

    Delimitar Editar a usuarios logeados

--- Tercera_preentrega_penaRafael/blogs/views.py
+++ Tercera_preentrega_penaRafael/blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
 from .forms import BlogForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def lista_blogs(request):
     blogs = Blog.objects.all()
@@ -20,20 +22,37 @@ def crear_blog(request):
         form = BlogForm()
     return render(request, 'blogs/formulario_blog.html', {'form': form})
 
+
 def editar_blog(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
-    if request.method == 'POST':
+
+    if blog.autor != request.user:
+        messages.error(request, "No tienes permiso para editar este blog.")
+        return redirect('lista_blogs')
+
+    if request.method == "POST":
         form = BlogForm(request.POST, request.FILES, instance=blog)
         if form.is_valid():
             form.save()
-            return redirect('detalle_blog', blog_id=blog.id)
+            messages.success(request, 'Blog actualizado exitosamente.')
+            return redirect('lista_blogs')
     else:
         form = BlogForm(instance=blog)
-    return render(request, 'blogs/formulario_blog.html', {'form': form})
+    
+    return render(request, 'blogs/formulario_blog.html', {'form': form, 'blog': blog})
+
 
 def eliminar_blog(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
-    if request.method == 'POST':
-        blog.delete()
+
+   
+    if blog.autor != request.user:
+        messages.error(request, "No tienes permiso para eliminar este blog.")
         return redirect('lista_blogs')
+
+    if request.method == "POST":
+        blog.delete()
+        messages.success(request, "Blog eliminado exitosamente.")
+        return redirect('lista_blogs')
+    
     return render(request, 'blogs/eliminar_blog.html', {'blog': blog})
